dfaffacb4813831654d5c4eddceecd25155e187f
psylon <stanislav.antunovich@yandex.ru>
2019-06-15 21:56:26 +0300

    tests init

---
+++ src/test/java/ru/geekbrains/lesson3/queue/QueueImpTest.java
@@ -0,0 +1,30 @@
+package ru.geekbrains.lesson3.queue;
+
+import org.junit.Test;
+
+public class QueueImpTest {
+
+    @Test
+    public void insert() {
+    }
+
+    @Test
+    public void remove() {
+    }
+
+    @Test
+    public void peek() {
+    }
+
+    @Test
+    public void size() {
+    }
+
+    @Test
+    public void isFull() {
+    }
+
+    @Test
+    public void isEmpty() {
+    }
+}
---
+++ src/test/java/ru/geekbrains/lesson3/stack/StackImplTest.java
@@ -0,0 +1,30 @@
+package ru.geekbrains.lesson3.stack;
+
+import org.junit.Test;
+
+public class StackImplTest {
+
+    @Test
+    public void push() {
+    }
+
+    @Test
+    public void peek() {
+    }
+
+    @Test
+    public void pop() {
+    }
+
+    @Test
+    public void size() {
+    }
+
+    @Test
+    public void isEmpty() {
+    }
+
+    @Test
+    public void isFull() {
+    }
+}
