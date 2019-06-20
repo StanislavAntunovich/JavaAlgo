ee4aa4f437be4d35ad84faf6a2c10b0b586617b7
psylon <stanislav.antunovich@yandex.ru>
2019-06-20 21:44:14 +0300

    1. Реализовать все классы, рассмотренные в данном уроке.

---
+++ src/main/java/ru/geekbrains/lesson4/LinkedList.java
@@ -0,0 +1,31 @@
+package ru.geekbrains.lesson4;
+
+public interface LinkedList<E> {
+
+    void insertFirst(E value);
+
+    E removeFirst();
+
+    boolean remove(E value);
+
+    boolean contains(E value);
+
+    boolean isEmpty();
+
+    int size();
+
+    void display();
+
+    E peek();
+
+    Entry getFirst();
+
+    class Entry<E> {
+        protected final E value;
+        protected Entry<E> next;
+
+        public Entry(E value) {
+            this.value = value;
+        }
+    }
+}
---
+++ src/main/java/ru/geekbrains/lesson4/LinkedQueueImpl.java
@@ -0,0 +1,27 @@
+package ru.geekbrains.lesson4;
+
+import ru.geekbrains.lesson3.queue.Queue;
+
+public class LinkedQueueImpl<E> extends LinkedListImpl<E> implements Queue<E> {
+
+    @Override
+    public boolean insert(E value) {
+        super.insertFirst(value);
+        return true;
+    }
+
+    @Override
+    public E remove() {
+        return super.removeFirst();
+    }
+
+    @Override
+    public boolean isFull() {
+        return false;
+    }
+
+    @Override
+    public void clear() {
+        firstElement = null;
+    }
+}
